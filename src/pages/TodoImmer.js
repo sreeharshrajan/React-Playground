13207ec40030e2f2d69136deda21f968eb682283
sreeharshrajan <sreeharshkrajan@gmail.com>
2021-11-10 23:43:53 +0530

    Todo Using Immer

---
+++ src/pages/TodoImmer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { produce } from "immer";
+const TodoImmer = () => {
+  return (
+    <div>
+      <h1>Todo Using Immer</h1>
+    </div>
+  );
+};
+
+export default TodoImmer;
